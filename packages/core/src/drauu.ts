7def2586c32d8ab11ff428120fb7ae6ef7e74ccd
Nils Bergmann <nilsbergmann@noim.io>
2025-12-04 11:54:09 +0100

    fix: Apple pencil with "Scribble" enabled

--- packages/core/src/drauu.ts
+++ packages/core/src/drauu.ts
@@ -86,6 +86,7 @@ export class Drauu {
     const move = this.eventMove.bind(this)
     const end = this.eventEnd.bind(this)
     const keyboard = this.eventKeyboard.bind(this)
+    const touchMove = this.touchMove.bind(this)
 
     target.addEventListener('pointerdown', start, { passive: false })
     listenWindow.addEventListener('pointermove', move, { passive: false })
@@ -93,6 +94,7 @@ export class Drauu {
     listenWindow.addEventListener('pointercancel', end, { passive: false })
     listenWindow.addEventListener('keydown', keyboard, false)
     listenWindow.addEventListener('keyup', keyboard, false)
+    listenWindow.addEventListener('touchmove', touchMove, { passive: false })
 
     this._disposables.push(() => {
       target.removeEventListener('pointerdown', start)
@@ -101,6 +103,7 @@ export class Drauu {
       listenWindow.removeEventListener('pointercancel', end)
       listenWindow.removeEventListener('keydown', keyboard, false)
       listenWindow.removeEventListener('keyup', keyboard, false)
+      listenWindow.removeEventListener('touchmove', touchMove)
     })
 
     this._emitter.emit('mounted')
@@ -200,6 +203,17 @@ export class Drauu {
     this._emitter.emit('end')
     this._emitter.emit('changed')
     this._originalPointerId = null
+  }
+
+  private touchMove(event: TouchEvent) {
+    const touches = Array.from(event.touches)
+    /**
+     * If you use an apple pencil on iPadOS and have "Scribble" enabled, you need to cancel the move event,
+     * otherwise drauu will only register every second stroke.
+     */
+    if (touches.some(touch => touch.touchType === 'stylus' && touch.identifier === this._originalPointerId)) {
+      event.preventDefault()
+    }
   }
 
   private acceptsInput(event: PointerEvent) {
